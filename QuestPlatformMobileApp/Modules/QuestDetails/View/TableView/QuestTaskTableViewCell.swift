b216173eecc3a28b7c58dd0187c39bff33c96efa
Anton Poltoratskyi <anton.poltoratskii@gmail.com>
2018-04-01 22:13:10 +0300

    Added task cell and table view delegate & datasource conformance.

---
+++ QuestPlatformMobileApp/Modules/QuestDetails/View/TableView/QuestTaskTableViewCell.swift
@@ -0,0 +1,80 @@
+//
+//  QuestTaskTableViewCell.swift
+//  QuestPlatformMobileApp
+//
+//  Created by Anton Poltoratskyi on 01.04.2018.
+//  Copyright © 2018 Anton Poltoratskyi. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+final class QuestTaskTableViewCell: UITableViewCell {
+    
+    // MARK: - Views
+    
+    private(set) lazy var cardView: QuestCardView = {
+        let cardView = QuestCardView()
+        contentView.addSubview(cardView)
+        return cardView
+    }()
+    
+    private(set) lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = Layout.Colors.titleColor
+        label.font = Layout.Fonts.titleFont
+        cardView.addSubview(label)
+        return label
+    }()
+    
+    
+    // MARK: - Init
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    
+    // MARK: - Setup
+    
+    private func setup() {
+        backgroundColor = .clear
+        
+        cardView.snp.makeConstraints { maker in
+            maker.edges.equalToSuperview()
+        }
+        
+        titleLabel.snp.makeConstraints { maker in
+            maker.leading.equalToSuperview().offset(Layout.titleLabel.horizontalInset)
+            maker.trailing.equalToSuperview().inset(Layout.titleLabel.horizontalInset)
+            maker.centerY.equalToSuperview()
+        }
+    }
+}
+
+// MARK: - Layout
+
+extension QuestTaskTableViewCell {
+    
+    enum Layout {
+        static let height: CGFloat = 64
+        
+        enum Colors {
+            static let titleColor = UIColor.darkText
+        }
+        
+        enum Fonts {
+            static let titleFont = UIFont.appFont(ofSize: 16, weight: .medium)
+        }
+        
+        enum titleLabel {
+            static let horizontalInset: CGFloat = 16
+        }
+    }
+}
--- QuestPlatformMobileApp/Modules/QuestDetails/View/ViewController/QuestDetailsViewController.swift
+++ QuestPlatformMobileApp/Modules/QuestDetails/View/ViewController/QuestDetailsViewController.swift
@@ -13,6 +13,8 @@ final class QuestDetailsViewController: UIViewController, View {
     typealias Output = QuestDetailsViewOutput
     var output: Output!
     
+    private var cells: [QuestTaskCellModel] = []
+    
     
     // MARK: - Views
     
@@ -31,10 +33,27 @@ final class QuestDetailsViewController: UIViewController, View {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        output.viewDidLoad()
     }
 }
 
 // MARK: - QuestDetailsViewInput
 extension QuestDetailsViewController: QuestDetailsViewInput {
     
+}
+
+// MARK: - Table View
+extension QuestDetailsViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cells.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withModel: cellModel(at: indexPath), for: indexPath)
+    }
+    
+    private func cellModel(at indexPath: IndexPath) -> QuestTaskCellModel {
+        return cells[indexPath.row]
+    }
 }
